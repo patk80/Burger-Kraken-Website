1c2ad34641ba4cfda4749c1b546e9a70b194679b
Pat Kelly <PatK80@gmail.com>
2021-01-04 19:38:19 -0500

    Condensing JS code - using function

--- scripts.js
+++ scripts.js
@@ -4,16 +4,12 @@ const hamburgerIcon = document.querySelector("#hamburger-icon");
 const bar1          = document.querySelector("#bar-1");
 const bar2          = document.querySelector("#bar-2");
 const bar3          = document.querySelector("#bar-3");
-
 const about         = document.querySelector("#link-1");
 const geography     = document.querySelector("#link-2");
 const funFacts      = document.querySelector("#link-3");
 
 toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-  bar1.classList.toggle("bar-1-active");
-  bar2.classList.toggle("bar-2-active");
-  bar3.classList.toggle("bar-3-active");
+  switchToggleStatus();
 });
 
 about.addEventListener("click", () => {
